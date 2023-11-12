58c5eb2a995e32b52572dfdce2be07e7421711f0
NOUR CHE <91270355+Nourche@users.noreply.github.com>
2023-11-12 14:17:29 +0000

    Update timeline

--- src/parts/timeline/timelineElements.js
+++ src/parts/timeline/timelineElements.js
@@ -20,35 +20,7 @@ let timelineElements = [
         "Turning designs into dynamic web experiences.",
       icon: "work",
     },
-    // {
-    //   id: 4,
-    //   title: "Oak Ridge College",
-    //   location: "South Warren, Geshington",
-    //   description:
-    //     "Online Course in Magical Beasts and Wonders of the World - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-    //   buttonText: "Course Certificate",
-    //   date: "September 2011",
-    //   icon: "school",
-    // },
-    // {
-    //   id: 5,
-    //   title: "Hawking College",
-    //   location: "Skystead, Craonia",
-    //   description:
-    //     "College - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-    //   buttonText: "College Projects",
-    //   date: "2007 - 2011",
-    //   icon: "school",
-    // },
-    // {
-    //   id: 6,
-    //   title: "Marble Hills Grammar School",
-    //   location: "Dragontail, Ascana",
-    //   description:
-    //     "Highschool - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-    //   date: "2003 - 2007",
-    //   icon: "school",
-    // },
+   
   ];
   
   export default timelineElements;
